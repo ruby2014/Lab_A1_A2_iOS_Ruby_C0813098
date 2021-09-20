ec669e527d89516d9cfc11d51712c5792b340c33
Ruby <mansishrma914@gmail.com>
2021-09-20 01:48:49 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Ruby_C0813098/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Ruby_C0813098
+//
+//  Created by Mac on 2021-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
